6bd4dee719f0ee42a0ff80e30deb13d478603448
judithlient <judith@rzilient.club>
2021-08-07 20:05:30 +0200

    Delete console log

--- src/utils/getNearbyPlaces.jsx
+++ src/utils/getNearbyPlaces.jsx
@@ -12,7 +12,6 @@ export default getNearbyPlaces;
 const getNearbyButtons = async (lat, lon, category, rad = 1000) => {
   const buttonsUrl = `${urls.buttonUrl}lat=${lat}&lon=${lon}&radius=${rad}&limit=15&idxSet=POI&categorySet=${category}&key=${process.env.REACT_APP_TOMTOM}`;
   const response = await axios.get(buttonsUrl);
-  console.log(response.data.results);
   return response.data.results;
 };
 

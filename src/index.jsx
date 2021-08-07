5545d7bce6f57baee2861d2b6454f2b4c94dc882
judithlient <judith@rzilient.club>
2021-08-07 20:04:59 +0200

    Remove files

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Map from './components/Main';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Map />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
